91f51aee94887a4a0139e8a784e02f62f776ce73
Matevos Ghazaryan <matevos@helix.am>
2019-10-11 18:00:35 +0400

    added new listeners for DMNetworkIStatusHandle

--- dmnetworking/src/main/java/com/dm/dmnetworking/DMNetworkIStatusHandleListener.java
+++ dmnetworking/src/main/java/com/dm/dmnetworking/DMNetworkIStatusHandleListener.java
@@ -6,9 +6,11 @@ public interface DMNetworkIStatusHandleListener {
 
     void onComplete(String status, JSONObject jsonObject);
 
-    default void onTokenUpdate() {
+    default void onTokenUpdate() {}
 
-    }
+    default void onForceUpdate(){}
+
+    default void onMaintenenceMode(){}
 
     default void onError(String status, JSONObject jsonObject) {
 
